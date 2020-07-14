67d54e8387cc3d08225ad7d002dd08ea0490b9a2
Sophie <sophiebrown438@gmail.com>
2020-07-14 21:10:38 +0100

    First pass test for deposit function

--- spec/transactions.test.js
+++ spec/transactions.test.js
@@ -1,3 +1,7 @@
-test('test', () =>{
-  expect(2).toBe(2)
+const deposit = require('../src/transactions.js')
+
+describe('#Transactions', () =>{
+  test('Allows a deposit', () => {
+    expect(typeof deposit(5)).toBe('number')
+  })
 })
---
+++ src/transactions.js
@@ -0,0 +1,5 @@
+function deposit(amount) {
+  return 5
+}
+
+module.exports = deposit;
