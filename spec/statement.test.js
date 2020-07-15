db17adac74b3c21d972bb1b36a6ec1b309c4d6dd
Sophie <sophiebrown438@gmail.com>
2020-07-15 16:21:30 +0100

    Pass test for Statement format function

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -4,4 +4,11 @@ let statements = new statement.statement
 describe('#Statement', () =>{
   test('Should return header', () => {
     expect(statements.header()).toBe(" Date      || Credit || Debit || Balance \n")
-  })})
+  })
+
+  test('Should return formatted statement', () => {
+    var n = new Date().toDateString()
+    // console.log(statements.format({"date": n, "credit": "£5", "debit": "------", "balance": "£5"}))
+    expect(statements.format([{"date": n, "credit": "£5", "debit": "------", "balance": "£5"}])).toMatch(`${n}  || £5  || ------  || £5`)
+  })
+})
--- src/statement.js
+++ src/statement.js
@@ -1,7 +1,18 @@
+const history = require('../src/transactionHistory.js')
+var transactions = new history.history
+
 class Statement {
   header(){
     return " Date      || Credit || Debit || Balance \n"
   }
-}
+
+  format(historytransaction){
+      let valArr = historytransaction.map(o => Object.values(o));
+      var final = valArr.join().replace(/,/g,"  || ");
+      console.log(this.header())
+      console.log(final)
+      return final
+    }
+  };
 
 module.exports.statement = Statement;
